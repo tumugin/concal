fd54164aca94cf90aa32ad774bfea75160c0fdac
Kazuki Oishi <oishikazuki@gmail.com>
2020-07-30 05:26:27 +0900

    カラム名を間違えていた

--- database/migrations/2020_07_30_000000_create_users_table.php
+++ database/migrations/2020_07_30_000000_create_users_table.php
@@ -14,7 +14,7 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id('screen_name')->primary();
+            $table->id('user_id')->primary();
             $table->string('screen_name', 20);
             $table->string('name', 100);
             $table->string('password', 512);
