da776eb14741549407b737597522ea0a583c39ca
Kazuki Oishi <oishikazuki@gmail.com>
2020-08-22 02:10:52 +0900

    テストを更新した2

---
+++ tests/Unit/User/AssertParamTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Tests\Unit\User;
+
+use App\Models\User;
+use Tests\TestCase;
+
+class AssertParamTest extends TestCase
+{
+    /**
+     * @dataProvider dataProviderSuccessCase
+     */
+    public function testAssertStrongPasswordSuccessPattern(string $test_password)
+    {
+        User::assertStrongPassword($test_password);
+        $this->assertTrue(true);
+    }
+
+    public function dataProviderSuccessCase(): array
+    {
+        return [
+            ['ErusaErusa12345'],
+            ['Erusa_Erusa12345'],
+            ['Erusa12345'],
+        ];
+    }
+
+    /**
+     * @dataProvider dataProviderFailCase
+     *
+     */
+    public function testAssertStrongPasswordFailPattern(string $test_password)
+    {
+        $this->expectException('InvalidArgumentException');
+        User::assertStrongPassword($test_password);
+    }
+
+    public function dataProviderFailCase(): array
+    {
+        return [
+            [''],
+            ['erusa_is_kawaii'],
+            ['amupi'],
+            ['erusa12345'],
+            ['Erusa1234'],
+        ];
+    }
+}
