f1fd1991eb36e636a88a74ef716ac47571acd6be
Kazuki Oishi <oishikazuki@gmail.com>
2020-08-22 06:19:21 +0900

    コントローラのテストを実装した

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/login', '\App\Http\Controllers\Api\ApiAuthController@login');
+Route::post('/login', '\App\Http\Controllers\Api\ApiAuthController@login');
 
-Route::middleware('auth:api')->get('/token/revoke', '\App\Http\Controllers\Api\ApiAuthController@revokeTokens');
+Route::middleware('auth:api')
+    ->post('/token/revoke', '\App\Http\Controllers\Api\ApiAuthController@revokeTokens');
---
+++ tests/Unit/Feature/ApiAuthControllerTest.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ApiAuthControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->setupPassport();
+    }
+
+    private const _TEST_USER_DATA = [
+        'user_name' => 'erusa',
+        'name' => 'エルサ',
+        'password' => 'Erusa12345',
+        'email' => 'erusa@example.com',
+    ];
+
+    public function testLoginWithEMail()
+    {
+        User::createUser(
+            self::_TEST_USER_DATA['user_name'],
+            self::_TEST_USER_DATA['name'],
+            self::_TEST_USER_DATA['password'],
+            self::_TEST_USER_DATA['email'],
+            User::USER_PRIVILEGE_USER
+        );
+        $response = $this->post(
+            '/api/login',
+            [
+                'email' => self::_TEST_USER_DATA['email'],
+                'password' => self::_TEST_USER_DATA['password'],
+            ]
+        );
+        $response->assertStatus(200);
+        $response->assertJsonStructure([
+            'apiToken',
+        ]);
+    }
+
+    public function testLoginWithUserName()
+    {
+        User::createUser(
+            self::_TEST_USER_DATA['user_name'],
+            self::_TEST_USER_DATA['name'],
+            self::_TEST_USER_DATA['password'],
+            self::_TEST_USER_DATA['email'],
+            User::USER_PRIVILEGE_USER
+        );
+        $response = $this->post(
+            '/api/login',
+            [
+                'userName' => self::_TEST_USER_DATA['user_name'],
+                'password' => self::_TEST_USER_DATA['password'],
+            ]
+        );
+        $response->assertStatus(200);
+        $response->assertJsonStructure([
+            'apiToken',
+        ]);
+    }
+
+    public function testLoginFailed()
+    {
+        factory(User::class)->create();
+        $response = $this->post(
+            '/api/login',
+            [
+                'userName' => 'hoge',
+                'password' => 'hoge',
+            ]
+        );
+        $response->assertStatus(401);
+        $response->assertJsonStructure([
+            'error',
+        ]);
+    }
+
+    public function testRevokeTokens()
+    {
+        $user = factory(User::class)->create();
+        $token = $user->createApiToken();
+        $response = $this
+            ->withToken($token)
+            ->post(
+                '/api/token/revoke',
+                []
+            );
+        $response->assertStatus(200);
+    }
+}
