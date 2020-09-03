406e0928b7fed2741435a43449ed1378c31abafb
Kazuki Oishi <oishikazuki@gmail.com>
2020-09-03 23:59:04 +0900

    入れ忘れていた設定周りを入れた

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import * as ReactDOM from 'react-dom'
 import React from 'react'
-import { PageRouter } from './router'
+import { PageRouter } from 'router'
 
 ReactDOM.render(<PageRouter />, document.getElementById('root'))
