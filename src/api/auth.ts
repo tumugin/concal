0bc62a416d38dc2201a712b876c55d5b81bc41bd
Kazuki Oishi <oishikazuki@gmail.com>
2020-09-08 01:45:11 +0900

    認証周りのAPIを実装した

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -6,12 +6,33 @@ interface LoginResponse {
     apiToken: string
 }
 
+interface SelfResponse {
+    info: {
+        id: string
+        userName: string
+        name: string
+        email: string
+        userPrivilege: string
+    }
+}
+
 export async function login({ email, userName, password }: { email?: string; userName?: string; password: string }) {
     try {
         const response = await Axios.post<LoginResponse>('/api/login', {
             email: email,
             userName: userName,
             password: password,
+        })
+        return response.data
+    } catch (e) {
+        throw generateError(e)
+    }
+}
+
+export async function self({ apiKey }: ApiKeyParam) {
+    try {
+        const response = await Axios.get<SelfResponse>('/api/self', {
+            headers: getAuthHeader(apiKey),
         })
         return response.data
     } catch (e) {
