48b88ef41cdf10cb2a5e045430c607416c93b49c
Kazuki Oishi <oishikazuki@gmail.com>
2020-09-12 18:40:05 +0900

    Voyagerを導入する

---
+++ app/Models/VoyagerAdminUser.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Models;
+
+class VoyagerAdminUser extends \TCG\Voyager\Models\User
+{
+
+}
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', '\App\Http\Controllers\Page\TopPageController@renderTopPage');
+
+
+Route::group(['prefix' => 'admin'], function () {
+    Voyager::routes();
+});
