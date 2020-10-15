57791989bd0b3d2c820d72a16277f41af97bad7d
Kazuki Oishi <oishikazuki@gmail.com>
2020-10-16 07:13:44 +0900

    項目を追加した

--- app/Http/Controllers/Api/TopContentsController.php
+++ app/Http/Controllers/Api/TopContentsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\CastAttend;
 use App\Models\StoreGroup;
 
 class TopContentsController extends Controller
@@ -11,18 +12,30 @@ class TopContentsController extends Controller
     {
         $store_groups = StoreGroup::query()
             ->with('stores')
-            ->limit(10);
-        $mapped_store_groups = $store_groups
+            ->limit(10)
             ->get()
             ->map(fn($item) => collect($item->getUserAttributes())
                 ->merge([
-                    'stores' => $item->stores->map(fn($store) => $store->getUserAttributes())
+                    'stores' => $item->stores->map(fn($store) => $store->getUserAttributes()),
+                ])
+            );;
+        $recent_updated_attends = CastAttend::query()
+            ->with('store')
+            ->with('store.storeGroup')
+            ->orderByDesc('id')
+            ->limit(10)
+            ->get()
+            ->map(fn($item) => collect($item->getUserAttributes())
+                ->merge([
+                    'store' => $item->store->getUserAttributes(),
+                    'storeGroup' => $item->store->storeGroup->getUserAttributes(),
                 ])
             );
         return [
             'success' => true,
             'data' => [
-                'storeGroups' => $mapped_store_groups,
+                'storeGroups' => $store_groups,
+                'recentUpdatedAttends' => $recent_updated_attends,
             ]
         ];
     }
