a7f6bf35264346f5ca9b636077e30d1bd0461d40
Kazuki Oishi <oishikazuki@gmail.com>
2020-11-01 05:55:46 +0900

    店舗ごとの出勤一覧ページを実装する

---
+++ app/Http/Controllers/Api/Stores/AttendsController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers\Api\Stores;
+
+use App\Http\Controllers\Controller;
+use App\Models\CastAttend;
+use App\Models\Store;
+use Illuminate\Http\Request;
+
+class AttendsController extends Controller
+{
+    public function index(Store $store, Request $request)
+    {
+        $request->validate([
+            'startDate' => 'required|date',
+            'endDate' => 'required|date',
+        ]);
+        $attends = CastAttend::whereStoreId($store->id)
+            ->whereBetween(
+                'start_time',
+                [$request->get('startDate'), $request->get('endDate')],
+            )
+            ->whereBetween(
+                'end_time',
+                [$request->get('startDate'), $request->get('endDate')]
+            )
+            ->with('cast')
+            ->get();
+        $mapped_attends = $attends->map(
+            fn(CastAttend $cast_attend) => collect($cast_attend->getUserAttributes())
+                ->merge([
+                    'cast' => $cast_attend->cast->getUserAttributes(),
+                ])
+        );
+        return [
+            'success' => true,
+            'data' => [
+                'attends' => $mapped_attends,
+            ]
+        ];
+    }
+}
