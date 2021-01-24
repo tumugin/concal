6920bb9bfdd614b0e48fd36abda002751161f0a2
Kazuki Oishi <oishikazuki@gmail.com>
2021-01-25 01:25:49 +0900

    よろしくなさそうなら処理を直した

--- app/Http/Transformers/Api/Admin/CastAttendShowTransformer.php
+++ app/Http/Transformers/Api/Admin/CastAttendShowTransformer.php
@@ -6,12 +6,24 @@ use App\Models\CastAttend;
 
 class CastAttendShowTransformer extends CastAttendTransformer
 {
-    public function transform(CastAttend $cast_attend): array
+    protected $defaultIncludes = [
+        'storeName',
+        'groupId',
+        'groupName',
+    ];
+
+    public function includeStoreName(CastAttend $cast_attend)
     {
-        return array_merge(parent::transform($cast_attend), [
-            'storeName' => $cast_attend->store->store_name,
-            'groupId' => $cast_attend->store->store_group_id,
-            'groupName' => $cast_attend->store->storeGroup->group_name,
-        ]);
+        $this->primitive($cast_attend->store->store_name);
+    }
+
+    public function includeGroupId(CastAttend $cast_attend)
+    {
+        $this->primitive($cast_attend->store->store_group_id);
+    }
+
+    public function includeGroupName(CastAttend $cast_attend)
+    {
+        $this->primitive($cast_attend->store->storeGroup->group_name);
     }
 }
